18eebc139798de86d356fa42bd2ef0fff9518063
Anna-Korchahina <korchagina.anna.v@gmail.com>
2023-12-16 17:27:04 +0200

    feat: do task dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+  for (let dom of domains) {
+
+    const arr = dom.split('.').reverse();
+
+    for (let i = 0; i < arr.length; i++) {
+      const way = '.' + arr.slice(0, i + 1).join('.');
+      if (result[way]) {
+        result[way] += 1;
+      } else {
+        result[way] = 1;
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
