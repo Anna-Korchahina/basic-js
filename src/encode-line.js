a066946cd42968ea6411c1e55a9a79ef60f1fcf9
Anna-Korchahina <korchagina.anna.v@gmail.com>
2023-12-16 17:28:20 +0200

    feat: do task encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str == '') {
+    return str;
+  }
+  let answ = '';
+  let prev = '';
+  let countEl = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === prev) {
+      countEl += 1;
+    } else {
+      countEl += 1;
+      if (countEl === 1) {
+        answ += prev;
+      } else {
+        answ += countEl + prev;
+      }
+      countEl = 0;
+      prev = str[i];
+    }
+    if (i === (str.length - 1)) {
+      countEl += 1;
+      if (countEl === 1) {
+        answ += prev;
+      } else {
+        answ += countEl + prev;
+      }
+    }
+
+  }
+  return answ;
 }
 
 module.exports = {
